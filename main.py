1dc5dbd3ca5addee30efced9b5b433588d3930ed
Mehmet Sabri <sudousermain@proton.me>
2026-02-23 13:03:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -233,16 +233,16 @@ async def birthday_loop():
                     if role and role in member.roles:
                         await member.remove_roles(role)
 
-# ================ BREAK ================
+# ================ MENTION LISTENER BREAK ================
 
-# ==========================
-# MENTION LISTENER
-#
-# SUDO TAG
-# ==========================
 SUDO_ID = 1288247401752166453
 HIMENO_ID = 1467405843065602141
 GIF_SUDO_TAG = "https://media.giphy.com/media/n7TMv8jwpKRA5HdVt2/giphy.gif"
+GIF_HIMENO_REPLY = "https://media.discordapp.net/stickers/1323198799191080960.webp?size=160&quality=lossless"
+
+# ==========================
+# SUDO TAG
+# ==========================
 
 @bot.event
 async def on_message(message):
@@ -264,6 +264,25 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
+# =============================
+# HIMENO REPLY RESPONSE
+# =============================
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    if message.reference and message.reference.resolved:
+        if message.reference.resolved.author.id == HIMENO_ID:
+            embed = discord.Embed(
+                description="what did you say?",
+                color=discord.Color.red()
+            )
+            embed.set_image(url=GIF_HIMENO_REPLY)
+            await message.channel.send(embed=embed)
+
+    await bot.process_commands(message)
 
 # =============================
 # RUN
