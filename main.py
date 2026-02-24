27b2dc60331cf06c04d736703639668756d99ce2
Mehmet Sabri <sudousermain@proton.me>
2026-02-24 03:48:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
-import os
-
-TOKEN = os.getenv("DISCORD_TOKEN")
+import asyncio
+from config import TOKEN
 
 intents = discord.Intents.default()
 intents.members = True
@@ -10,19 +9,22 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f"✅ Logged in as {bot.user}")
 
+
 async def load_cogs():
     await bot.load_extension("cogs.time_cog")
     await bot.load_extension("cogs.reply_cog")
+
 
 async def main():
     async with bot:
         await load_cogs()
         await bot.start(TOKEN)
 
+
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
