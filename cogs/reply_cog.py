1b64858768cea8276d71199ab5dfd3c178d83349
Mehmet Sabri <sudousermain@proton.me>
2026-02-24 04:16:35 -0500

    Update reply_cog.py

--- cogs/reply_cog.py
+++ cogs/reply_cog.py
@@ -6,15 +6,20 @@ from config import SUDO_ID, HIMENO_ID, GIF_SUDO_TAG, GIF_HIMENO_REPLY
 class ReplyCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        print("ReplyCog loaded")
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
             return
 
-        # HIMENO reply
+        # DEBUG
+        print("Message received:", message.content)
+
+        # HIMENO reply detection
         if message.reference and message.reference.resolved:
-            if message.reference.resolved.author.id == HIMENO_ID:
+            replied_author = message.reference.resolved.author
+            if replied_author and replied_author.id == HIMENO_ID:
                 embed = discord.Embed(
                     description="what did you say?",
                     color=discord.Color.red()
@@ -22,7 +27,7 @@ class ReplyCog(commands.Cog):
                 embed.set_image(url=GIF_HIMENO_REPLY)
                 await message.channel.send(embed=embed)
 
-        # SUDO tag
+        # SUDO mention detection
         if not message.reference:
             if any(user.id == SUDO_ID for user in message.mentions):
                 embed = discord.Embed(
