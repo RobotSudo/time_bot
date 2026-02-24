2a7613e7d7fc90a7f2249b0e54c5ebb0c68d9c42
Mehmet Sabri <sudousermain@proton.me>
2026-02-24 03:49:21 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,25 @@
+import os
 
+# =============================
+# TOKENS / DATABASE
+# =============================
+TOKEN = os.getenv("DISCORD_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# =============================
+# BIRTHDAY SETTINGS
+# =============================
+BIRTHDAY_ROLE_NAME = "Birthday guy"
+BIRTHDAY_CHANNEL_ID = 1468166938386497670
+
+# =============================
+# SPECIAL USERS
+# =============================
+SUDO_ID = 1288247401752166453
+HIMENO_ID = 1467405843065602141
+
+# =============================
+# GIFS
+# =============================
+GIF_SUDO_TAG = "https://media.giphy.com/media/n7TMv8jwpKRA5HdVt2/giphy.gif"
+GIF_HIMENO_REPLY = "https://media.discordapp.net/stickers/1323198799191080960.webp?size=160&quality=lossless"
